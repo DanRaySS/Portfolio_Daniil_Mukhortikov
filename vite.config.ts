88f8fbb5ae4f7f47839d9df532640195428eead0
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-07-16 20:41:41 +0500

    add svgr
     for transform svg into React comps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [svgr({
+    svgrOptions: {
+      plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
+      svgoConfig: {
+        floatPrecision: 2,
+      },
+    },
+    include: "**/*.svg?react",
+    exclude: "",
+  }), react()],
+  resolve: {
+    alias: {
+      app: "/src/app",
+      assets: "/src/assets",
+      entities: "/src/entities",
+      helpers: "/src/helpers",
+      pages: "/src/pages",
+      processes: "/src/processes",
+      shared: "/src/shared",
+      widgets: "/src/widgets",
+      features: "/src/features",
+    }
+  }
 })
