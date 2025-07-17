c669370a746669ed931718cb7ebf3dd300208294
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-07-17 18:54:44 +0500

    add Avatar

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { Avatar } from "shared/ui/Avatar/Avatar";
 import { Header } from "widgets/Header/Header";
 import cls from "./MainPage.module.scss";
 
@@ -9,7 +10,10 @@ interface IMainPageProps {
 export const MainPage = ({ className }: IMainPageProps) => {
   return (
     <div className={classNames(cls.MainPage, {}, [className])}>
-      <Header className={cls.Header}/>
+      <Header className={cls.Header} />
+      <Avatar className={cls.Avatar} />
+      <span>I do code and
+        make content about it!</span>
     </div>
   );
 };
---
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -0,0 +1,14 @@
+import classNames from "classnames";
+import cls from "./Avatar.module.scss";
+
+interface IAvatarProps {
+  className?: string;
+}
+
+export const Avatar = ({ className }: IAvatarProps) => {
+  return (
+    <div className={classNames(cls.Avatar, {}, [className])}>
+        <img className={cls.Avatar__img} src="/avatar.jpg" alt="avatar" />
+    </div>
+  );
+};
